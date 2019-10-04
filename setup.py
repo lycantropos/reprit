e2c369d863f9dfbab0d107ea0a937e4d56402286
Ibrakov Azat <azatibrakov@gmail.com>
2019-10-04 20:46:59 +0700

    Complete metadata

--- setup.py
+++ setup.py
@@ -16,9 +16,19 @@ tests_require = [
     'hypothesis>=3.73.1',
 ]
 
-setup(name='reprit',
+setup(name=reprit.__name__,
       packages=find_packages(exclude=('tests', 'tests.*')),
       version=reprit.__version__,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+      ],
+      license='MIT License',
       description=reprit.__doc__,
       long_description=Path('README.md').read_text(encoding='utf-8'),
       long_description_content_type='text/markdown',
