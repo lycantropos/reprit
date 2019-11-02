ac6cde1e9859f98d08dcd9a7e27e1db220b7ec0d
Ibrakov Azat <azatibrakov@gmail.com>
2019-11-03 01:47:43 +0300

    Complete test for evaluation

--- tests/base_tests/test_complex.py
+++ tests/base_tests/test_complex.py
@@ -9,7 +9,7 @@ from hypothesis import given
 from reprit import seekers
 from reprit.base import generate_repr
 from tests import strategies
-from tests.utils import Domain
+from tests.utils import Domain, to_namespace
 
 
 @given(strategies.complex_classes, strategies.booleans, strategies.booleans)
@@ -44,17 +44,23 @@ def test_call(class_with_instance: Tuple[Type[Domain], Domain],
 @pytest.mark.skipif(platform.python_implementation() == 'PyPy'
                     and sys.version_info > (3, 5, 3),
                     reason='Unreproducible failures on PyPy3.5.3')
-@given(strategies.complex_classes_with_instances, strategies.booleans)
+@given(strategies.complex_classes_with_instances, strategies.booleans,
+       strategies.booleans)
 def test_evaluation(class_with_instance: Tuple[Type[Domain], Domain],
-                    prefer_keyword: bool) -> None:
+                    prefer_keyword: bool,
+                    with_module_name: bool) -> None:
     cls, instance = class_with_instance
 
     repr_ = generate_repr(cls.__init__,
                           field_seeker=seekers.complex_,
-                          prefer_keyword=prefer_keyword)
+                          prefer_keyword=prefer_keyword,
+                          with_module_name=with_module_name)
     instance_repr = repr_(instance)
 
-    result = eval(instance_repr, {cls.__name__: cls})
+    result = eval(instance_repr,
+                  to_namespace(cls.__module__ + '.' + cls.__qualname__, cls)
+                  if with_module_name
+                  else to_namespace(cls.__qualname__, cls))
 
     assert vars(instance) == vars(result)
 
--- tests/base_tests/test_simple.py
+++ tests/base_tests/test_simple.py
@@ -8,7 +8,7 @@ from hypothesis import given
 
 from reprit.base import generate_repr
 from tests import strategies
-from tests.utils import Domain
+from tests.utils import Domain, to_namespace
 
 
 @given(strategies.simple_classes, strategies.booleans, strategies.booleans)
@@ -41,16 +41,22 @@ def test_call(class_with_instance: Tuple[Type[Domain], Domain],
 @pytest.mark.skipif(platform.python_implementation() == 'PyPy'
                     and sys.version_info > (3, 5, 3),
                     reason='Unreproducible failures on PyPy3.5.3')
-@given(strategies.simple_classes_with_instances, strategies.booleans)
+@given(strategies.simple_classes_with_instances, strategies.booleans,
+       strategies.booleans)
 def test_evaluation(class_with_instance: Tuple[Type[Domain], Domain],
-                    prefer_keyword: bool) -> None:
+                    prefer_keyword: bool,
+                    with_module_name: bool) -> None:
     cls, instance = class_with_instance
 
     repr_ = generate_repr(cls.__init__,
-                          prefer_keyword=prefer_keyword)
+                          prefer_keyword=prefer_keyword,
+                          with_module_name=with_module_name)
     instance_repr = repr_(instance)
 
-    result = eval(instance_repr, {cls.__name__: cls})
+    result = eval(instance_repr,
+                  to_namespace(cls.__module__ + '.' + cls.__qualname__, cls)
+                  if with_module_name
+                  else to_namespace(cls.__qualname__, cls))
 
     assert vars(result) == vars(instance)
 
