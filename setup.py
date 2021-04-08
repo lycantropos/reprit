5a1360c60968ae27db00ee901d2c7ce36c3c49da
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-08 22:41:59 +0300

    Complete 'setup.py' script

--- setup.py
+++ setup.py
@@ -32,5 +32,4 @@ setup(name=reprit.__name__,
       license='MIT License',
       url=project_base_url,
       download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.5.3',
-      install_requires=Path('requirements.txt').read_text(encoding='utf-8'))
+      python_requires='>=3.5.3')
