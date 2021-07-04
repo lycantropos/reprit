119a9b042987b87ea8b6b26a3a9635d73902ab61
Azat Ibrakov <azatibrakov@gmail.com>
2021-07-04 22:57:13 +0300

    Complete test for 'generate_repr' function

--- tests/base_tests/test_complex.py
+++ tests/base_tests/test_complex.py
@@ -17,15 +17,17 @@ from tests.utils import (ClassMethodInstance,
 
 
 @given(strategies.complex_classes_methods, strategies.argument_serializers,
-       strategies.booleans, strategies.booleans)
+       strategies.booleans, strategies.booleans, strategies.booleans)
 def test_basic(method: Method,
                argument_serializer: ArgumentSerializer,
                prefer_keyword: bool,
+               skip_defaults: bool,
                with_module_name: bool) -> None:
     result = generate_repr(method,
                            argument_serializer=argument_serializer,
                            field_seeker=seekers.complex_,
                            prefer_keyword=prefer_keyword,
+                           skip_defaults=skip_defaults,
                            with_module_name=with_module_name)
 
     assert callable(result)
@@ -33,10 +35,11 @@ def test_basic(method: Method,
 
 @given(strategies.complex_classes_with_methods_and_instances,
        strategies.argument_serializers, strategies.booleans,
-       strategies.booleans)
+       strategies.booleans, strategies.booleans)
 def test_call(class_method_instance: ClassMethodInstance,
               argument_serializer: ArgumentSerializer,
               prefer_keyword: bool,
+              skip_defaults: bool,
               with_module_name: bool) -> None:
     cls, method, instance = class_method_instance
 
@@ -44,6 +47,7 @@ def test_call(class_method_instance: ClassMethodInstance,
                           argument_serializer=argument_serializer,
                           field_seeker=seekers.complex_,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=with_module_name)
 
     result = repr_(instance)
@@ -55,9 +59,10 @@ def test_call(class_method_instance: ClassMethodInstance,
                     and sys.version_info > (3, 5, 3),
                     reason='Unreproducible failures on PyPy3.5.3')
 @given(strategies.complex_classes_with_methods_and_instances,
-       strategies.booleans, strategies.booleans)
+       strategies.booleans, strategies.booleans, strategies.booleans)
 def test_evaluation(class_with_method_and_instance: ClassMethodInstance,
                     prefer_keyword: bool,
+                    skip_defaults: bool,
                     with_module_name: bool) -> None:
     cls, method, instance = class_with_method_and_instance
 
@@ -65,6 +70,7 @@ def test_evaluation(class_with_method_and_instance: ClassMethodInstance,
                           argument_serializer=serializers.complex_,
                           field_seeker=seekers.complex_,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=with_module_name)
     instance_repr = repr_(instance)
 
@@ -79,15 +85,18 @@ def test_evaluation(class_with_method_and_instance: ClassMethodInstance,
 
 
 @given(strategies.unsupported_complex_classes_with_methods_and_instances,
-       strategies.argument_serializers, strategies.booleans)
+       strategies.argument_serializers, strategies.booleans,
+       strategies.booleans)
 def test_unsupported(class_with_method_and_instance: ClassMethodInstance,
                      argument_serializer: ArgumentSerializer,
+                     skip_defaults: bool,
                      prefer_keyword: bool) -> None:
     cls, method, instance = class_with_method_and_instance
 
     repr_ = generate_repr(method,
                           argument_serializer=argument_serializer,
                           field_seeker=seekers.complex_,
+                          skip_defaults=skip_defaults,
                           prefer_keyword=prefer_keyword)
 
     with pytest.raises(AttributeError):
@@ -95,9 +104,11 @@ def test_unsupported(class_with_method_and_instance: ClassMethodInstance,
 
 
 @given(strategies.complex_classes_with_methods_and_instances,
-       strategies.argument_serializers, strategies.booleans)
+       strategies.argument_serializers, strategies.booleans,
+       strategies.booleans)
 def test_with_module_name(class_with_method_and_instance: ClassMethodInstance,
                           argument_serializer: ArgumentSerializer,
+                          skip_defaults: bool,
                           prefer_keyword: bool) -> None:
     cls, method, instance = class_with_method_and_instance
 
@@ -105,6 +116,7 @@ def test_with_module_name(class_with_method_and_instance: ClassMethodInstance,
                           argument_serializer=argument_serializer,
                           field_seeker=seekers.complex_,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=True)
 
     result = repr_(instance)
--- tests/base_tests/test_simple.py
+++ tests/base_tests/test_simple.py
@@ -16,14 +16,16 @@ from tests.utils import (ClassMethodInstance,
 
 
 @given(strategies.simple_classes_methods, strategies.argument_serializers,
-       strategies.booleans, strategies.booleans)
+       strategies.booleans, strategies.booleans, strategies.booleans)
 def test_basic(method: Method,
                argument_serializer: ArgumentSerializer,
                prefer_keyword: bool,
+               skip_defaults: bool,
                with_module_name: bool) -> None:
     result = generate_repr(method,
                            argument_serializer=argument_serializer,
                            prefer_keyword=prefer_keyword,
+                           skip_defaults=skip_defaults,
                            with_module_name=with_module_name)
 
     assert callable(result)
@@ -31,16 +33,18 @@ def test_basic(method: Method,
 
 @given(strategies.simple_classes_with_methods_and_instances,
        strategies.argument_serializers, strategies.booleans,
-       strategies.booleans)
+       strategies.booleans, strategies.booleans)
 def test_call(class_with_method_and_instance: ClassMethodInstance,
               argument_serializer: ArgumentSerializer,
               prefer_keyword: bool,
+              skip_defaults: bool,
               with_module_name: bool) -> None:
     _, method, instance = class_with_method_and_instance
 
     repr_ = generate_repr(method,
                           argument_serializer=argument_serializer,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=with_module_name)
 
     result = repr_(instance)
@@ -52,15 +56,17 @@ def test_call(class_with_method_and_instance: ClassMethodInstance,
                     and sys.version_info > (3, 5, 3),
                     reason='Unreproducible failures on PyPy3.5.3')
 @given(strategies.simple_classes_with_methods_and_instances,
-       strategies.booleans, strategies.booleans)
+       strategies.booleans, strategies.booleans, strategies.booleans)
 def test_evaluation(class_with_method_and_instance: ClassMethodInstance,
                     prefer_keyword: bool,
+                    skip_defaults: bool,
                     with_module_name: bool) -> None:
     cls, method, instance = class_with_method_and_instance
 
     repr_ = generate_repr(method,
                           argument_serializer=serializers.complex_,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=with_module_name)
     instance_repr = repr_(instance)
 
@@ -75,15 +81,18 @@ def test_evaluation(class_with_method_and_instance: ClassMethodInstance,
 
 
 @given(strategies.simple_classes_with_methods_and_instances,
-       strategies.argument_serializers, strategies.booleans)
+       strategies.argument_serializers, strategies.booleans,
+       strategies.booleans)
 def test_with_module_name(class_with_method_and_instance: ClassMethodInstance,
                           argument_serializer: ArgumentSerializer,
-                          prefer_keyword: bool) -> None:
+                          prefer_keyword: bool,
+                          skip_defaults: bool) -> None:
     cls, method, instance = class_with_method_and_instance
 
     repr_ = generate_repr(method,
                           argument_serializer=argument_serializer,
                           prefer_keyword=prefer_keyword,
+                          skip_defaults=skip_defaults,
                           with_module_name=True)
 
     result = repr_(instance)
